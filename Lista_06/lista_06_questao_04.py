d65243eb38fafaa2515b888f31284628bc63c0d5
Charles Cesar Magno de Freitas <ccmfreitas@gmail.com>
2018-09-10 22:06:21 -0300

    Ajuste no algoritmo de Fibonacci (Lista 06 - Questão 04)

--- Lista_06/lista_06_questao_04.py
+++ Lista_06/lista_06_questao_04.py
@@ -3,10 +3,11 @@ valor_n = int(input('Informe o Valor de N: '))
 contador = 1
 valor_anterior = 0
 valor_atual = 1
+fibonacci = 1
 
 while (contador <= valor_n):
-   fibonacci = valor_anterior + valor_atual
    print(fibonacci)
-   valor_atual = valor_anterior
-   valor_anterior = fibonacci
+   fibonacci = valor_atual + valor_anterior
+   valor_anterior = valor_atual
+   valor_atual = fibonacci
    contador = contador + 1
