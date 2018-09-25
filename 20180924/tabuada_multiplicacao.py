42a4d9a648961349a6e37bd4723678a177de0131
Charles Cesar Magno de Freitas <ccmfreitas@gmail.com>
2018-09-24 21:02:58 -0300

    Corrigindo a Tabuada

--- 20180924/tabuada_multiplicacao.py
+++ 20180924/tabuada_multiplicacao.py
@@ -1,6 +1,6 @@
 valor_n = int(input('Informe um número entre 1 e 9: '))
 
-if (valor_n) in range(1,10):
+if (valor_n) in range(1,11):
    for contador in range (1, 11):
       print('{0} x {1} = {2}'.format(valor_n, contador, valor_n*contador))
 else:
