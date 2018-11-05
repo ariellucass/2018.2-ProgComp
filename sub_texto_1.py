2284375a77ac54ade80a7ac6f0384c9762fd03d3
Charles Cesar Magno de Freitas <ccmfreitas@gmail.com>
2018-11-05 08:03:33 -0300

    Manipulando Textos

---
+++ sub_texto_1.py
@@ -0,0 +1,22 @@
+varTexto_1 = 'Rio Grande do Norte;'
+
+varTexto_2 = varTexto_1[6]
+varTexto_3 = varTexto_1[6:]
+varTexto_4 = varTexto_1[:6]
+varTexto_5 = varTexto_1[6:12]
+varTexto_6 = varTexto_1[:-1]
+varTexto_7 = varTexto_1[:-1:2]
+
+print(varTexto_1)
+print('')
+print(varTexto_2)
+print('')
+print(varTexto_3)
+print('')
+print(varTexto_4)
+print('')
+print(varTexto_5)
+print('')
+print(varTexto_6)
+print('')
+print(varTexto_7)
---
+++ sub_texto_2.py
@@ -0,0 +1,17 @@
+varTexto_1 = 'Rio Grande do Norte#Natal#RN#Nordeste'
+
+nomeEstado, capitalEstado, siglaEstado, regiao = varTexto_1.split('#')
+
+varTexto_2 = varTexto_1.split('#')
+
+print(varTexto_1)
+print('')
+print(nomeEstado)
+print('')
+print(capitalEstado)
+print('')
+print(siglaEstado)
+print('')
+print(regiao)
+print('')
+print(varTexto_2)
