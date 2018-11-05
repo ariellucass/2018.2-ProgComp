f9252112c754c335db6f750f4b3ce08d0f8c263b
Charles Cesar Magno de Freitas <ccmfreitas@gmail.com>
2018-11-05 07:59:38 -0300

    Aulas do dia 31/10/2018 e 01/11/2018 (manipulando arquivos)

--- 20181031 - Manipulando Arquivos/gerar_medias_v2.py
+++ 20181031 - Manipulando Arquivos/gerar_medias_v2.py
@@ -1,22 +1,26 @@
 # Abrindo arquivo em modo de Leitura
 arquivo = open('notas_alunos.txt', 'r')
 
-lista_notas =  []
+# Armazenando o conteúdo do arquivo na variável
+
+lista_notas = []
 
 # Lendo a primeira linha do arquivo
 conteudo = arquivo.readline()
 
 # Adicionando a primeira linha do arquivo na lista_notas
-# Removendo '\n' com [:-1]
-linha = conteudo[:-1]
+# Removendo '\n' com [:-1] e separando as informações
+# com split (caractere de separação ';')
+linha = conteudo[:-1].split(';')
 lista_notas.append(linha)
 
 while conteudo:
    # Lendo uma linha do arquivo a cada iteração do WHILE
    conteudo = arquivo.readline()
    # Adicionando a linha do arquivo na lista_notas
-   # Removendo '\n' com [:-1]
-	linha = conteudo[:-1]
+   # Removendo '\n' com [:-1] e separando as informações
+   # com split (caractere de separação ';'
+   linha = conteudo[:-1].split(';')
    lista_notas.append(linha)
 
 # Fechando o Arquivo
@@ -26,3 +30,4 @@ arquivo.close()
 lista_notas.pop()
 
 print(lista_notas)
+
