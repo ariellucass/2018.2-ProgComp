1754082fc89a28da69a211a51f82c3f51a371db4
Charles Cesar Magno de Freitas <ccmfreitas@gmail.com>
2018-12-06 19:18:05 -0300

    Exemplos Dicionários

---
+++ 20181206 - Dicionários/exemplo_00.py
@@ -0,0 +1,8 @@
+frutas = [ ['laranja', 10, 1.50], ['manga', 30, 2.25], ['abacaxi', 25, 1.25], ['melancia', 15, 3.45]]
+
+nome_fruta = input('informe a fruta: ')
+
+#estoque_fruta = 
+#valor_venda = 
+
+print('O estoque da fruta {0} é {1} e o preço de venda é {2}'.format(nome_fruta,estoque_fruta, valor_venda)) 
